f40c79073ae6f00e906b438d5c6d2039e0de9fff
Tran Minh Hieu <tranminhhieu@Trans-MacBook-Pro.local>
2026-04-19 10:58:19 +0700

    [first]: Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  protected readonly title = signal('Hello my first Angular app with standalone components');
+
+  protected readonly imageUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
+
+  protected isDisabled = false;
+  protected isActive = false;
+  protected fruit = 'apple';
+  protected userName = 'Hieu'
+
+  buttonxClicked() {
+    console.log('Button clicked');
+  }
+
+  keyEnter(event: Object) {
+    console.log('Key enter: ', event);
+  }
+
+  updateUserName(username:HTMLInputElement) {
+    this.userName = username.value;
+    console.log('Username updated: ', this.userName);
+  }
 }
